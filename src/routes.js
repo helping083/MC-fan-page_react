d69d7d5778af21d170c3153208e3f85e53ba2117
helping083 <helping083@gmail.com>
2019-05-21 19:07:07 +0300

    init routes and the header

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from './HOC/Layout';
+const Routes = (props) => {
+    return (
+        <div>
+            <Layout/>
+        </div>
+    );
+};
+
+export default Routes;
