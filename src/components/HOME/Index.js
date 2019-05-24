0b146c25153ac24d8f858c25b3c2ca9ce74e6b76
helping083 <helping083@gmail.com>
2019-05-24 19:30:47 +0300

    start result page

--- src/components/HOME/Index.js
+++ src/components/HOME/Index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Featured from './Featured'
+import Matches from './matches/index';
+
 
 const Home = () => {
     return (
         <div className="bck_blue">
             <Featured/>
+            <Matches/>
         </div>
     );
 };
---
+++ src/components/UI/misc.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { link } from 'react-router-dom';
+
+export const Tag = ()=>{
+    const template = <div>tag</div>
+}
