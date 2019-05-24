2273ebe148fdbc89ba9d159383c4cce787e9890f
helping083 <helping083@gmail.com>
2019-05-24 12:10:02 +0300

    animation block routes

--- src/HOC/Layout.js
+++ src/HOC/Layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header-footer/Header';
 import Footer from '../components/Header-footer/Footer';
 
+
 const Layout = (props) => {
     return (
         <div>
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import Layout from './HOC/Layout';
+import Home from './components/HOME/Index';
+import {Switch, Route} from 'react-router-dom';
+
 const Routes = (props) => {
     return (
         <div>
-            <Layout/>
+            <Layout>
+              <Switch>
+                <Route exact component={Home} path="/"/>
+              </Switch>
+            </Layout>
         </div>
     );
 };
