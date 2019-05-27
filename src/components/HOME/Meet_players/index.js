edc088ab1811dd7ace4555018a61e8543b7507d8
helping083 <helping083@gmail.com>
2019-05-27 12:16:19 +0300

    meet the player star page

--- src/components/HOME/Meet_players/index.js
+++ src/components/HOME/Meet_players/index.js
@@ -12,7 +12,71 @@ class MeetPlayers extends Component {
     render() {
         return (
             <div className="home_meetplayers" style={{background: `#fff url(${Stripes})`}}>
-                s
+                <div className="container">
+                    <div className="home_meetplayers_wrapper">
+                        <div className="home_card_wrapper">
+                            card
+                        </div>
+                        <div className="home_text_wrapper">
+                            <div>
+                                <Tag
+                                    bck="#0e1731"
+                                    size="100px"
+                                    color="white"
+                                    add={{
+                                        display:'inline-block',
+                                        marginBottom: '20px'
+                                    }}
+                                >
+                                    Meet
+                                </Tag>
+                            </div>
+                            <div>
+                                <Tag
+                                    bck="#0e1731"
+                                    size="100px"
+                                    color="white"
+                                    add={{
+                                        display:'inline-block',
+                                        marginBottom: '20px'
+                                    }}
+                                >
+                                    the
+                                </Tag>
+                            </div>
+                            <div>
+                                <Tag
+                                    bck="#0e1731"
+                                    size="100px"
+                                    color="white"
+                                    add={{
+                                        display:'inline-block',
+                                        marginBottom: '20px'
+                                    }}
+                                >
+                                    players
+                                </Tag>
+                            </div>
+                            <div>
+                                <Tag 
+                                    bck="white"
+                                    size="27px"
+                                    color="#0e1731"
+                                    link={true}
+                                    linkto="/the_team"
+                                    add={{
+                                        display:'inline-block',
+                                        marginBottom: '27px',
+                                        border: '1px solid #0e1731',
+                                        textTransform: 'uppercase'
+                                    }}
+                                >
+                                    meet here
+                                </Tag>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
